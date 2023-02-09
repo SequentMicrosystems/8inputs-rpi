a44744e6a887887cf94b121274b27862dc13ad20
Jiri Pavlicek <jiri@pavlicek.cz>
2023-02-09 21:28:55 +0100

    fix python test

--- python/tests.py
+++ python/tests.py
@@ -3,6 +3,6 @@ import lib8inputs as i
 print("============================")
 print("read all inputs = " + str(i.get_opto_all(0)))
 print("============================")
-for i in range(8):
-    opt = r.get_opto(0, i+1)
-    print("input " + str(i+1) + " status = " + str(opt))
+for j in range(8): 
+    opt = i.get_opto(0, j+1) 
+    print("input " + str(j+1) + " status = " + str(opt))
